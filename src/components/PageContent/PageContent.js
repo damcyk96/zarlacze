acb00fa8b7dd9e856640b8bb971fe5f9b2c36974
Damian Obajtek <damian.obajtek@aexol.com>
2021-10-25 16:59:18 +0200

    fix

--- src/components/PageContent/PageContent.js
+++ src/components/PageContent/PageContent.js
@@ -2,14 +2,8 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Bundles from '../../pages/Bundles/Bundles'
 import EntriesWithDatePicker from '../../pages/EntriesWithDatePicker/EntriesWithDatePicker'
-import Settings from '../../pages/Settings'
 import Login from '../Login'
-<<<<<<< HEAD
-=======
-import EntriesWithDatePicker from '../../pages/EntriesWithDatePicker'
-import Bundles from '../Bundles'
 import BundlesSettings from '../../pages/BundlesSettings'
->>>>>>> b4bf01a1e21b1c87a109c9ba8e27f918839de621
 
 const PageContent = () => {
   return (
@@ -17,11 +11,7 @@ const PageContent = () => {
       <Route path="/" exact component={EntriesWithDatePicker} />
       <Route path="/login" component={Login} />
       <Route path="/bundles" component={Bundles} />
-<<<<<<< HEAD
-      <Route path="/settings" component={Settings} />
-=======
       <Route path="/settings" component={BundlesSettings} />
->>>>>>> b4bf01a1e21b1c87a109c9ba8e27f918839de621
     </Switch>
   )
 }
