45315588730f4bb0a1683405f909f87e8c307ddf
RyczekMateusz <ryczek.mateusz@gmail.com>
2021-10-25 10:12:53 +0200

    Routing to settings

--- src/components/PageContent/PageContent.js
+++ src/components/PageContent/PageContent.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import Login from '../Login'
 import EntriesWithDatePicker from '../../pages/EntriesWithDatePicker'
 import Bundles from '../Bundles'
+import BundlesSettings from '../../pages/BundlesSettings'
 
 const PageContent = () => {
   return (
@@ -10,6 +11,7 @@ const PageContent = () => {
       <Route path="/" exact component={EntriesWithDatePicker} />
       <Route path="/login" component={Login} />
       <Route path="/bundles" component={Bundles} />
+      <Route path="/settings" component={BundlesSettings} />
     </Switch>
   )
 }
