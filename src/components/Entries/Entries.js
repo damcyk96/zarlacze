d6940ee82b2e0cb8fea26fa052527eb529d87d63
Damian Obajtek <damian.obajtek@aexol.com>
2021-10-26 21:05:42 +0200

    draggable

--- src/components/Entries/Entries.js
+++ src/components/Entries/Entries.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, Button, Stack } from '@mui/material'
 import DateAdapter from '@mui/lab/AdapterDateFns'
 import { LocalizationProvider } from '@mui/lab'
@@ -8,6 +8,7 @@ import { Box } from '@mui/system'
 import { useMutation, gql } from '@apollo/client'
 import useGetEntriesByDate from '../../graphql/queries/useGetEntriesByDate'
 import { GET_ALL_ENTRIES } from '../../graphql/queries/useGetAllEntries'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
 const DELETE_ENTRY = gql`
   mutation DeleteEntry($_id: MongoID!) {
@@ -19,37 +20,97 @@ const DELETE_ENTRY = gql`
 
 const Entries = () => {
   const { data, loading } = useGetEntriesByDate()
+  const [entries, setEntries] = useState()
   const [deleteEntry] = useMutation(DELETE_ENTRY, {
     refetchQueries: [GET_ALL_ENTRIES, 'GetAllEntries'],
   })
 
+  useEffect(() => {
+    setEntries(data)
+  }, [data])
   if (loading) return <Loader />
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list)
+    const [removed] = result.splice(startIndex, 1)
+    result.splice(endIndex, 0, removed)
+
+    return result
+  }
+
+  const handleDragEnd = (result) => {
+    if (!result.destination) {
+      return
+    }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    )
+    console.log(this.state.items)
+    this.setState({
+      items,
+    })
+  }
 
   return (
     <LocalizationProvider dateAdapter={DateAdapter}>
-      <Container>
-        <h1>My entries</h1>
-        {data?.map((singleEntry) => (
-          <Box display="flex" justifyContent="center" key={singleEntry._id}>
-            <Stack direction="row" spacing={2}>
-              <SingleEntry singleEntry={singleEntry} />
-              <Button
-                variant="outlined"
-                color="error"
-                onClick={() => {
-                  deleteEntry({
-                    variables: {
-                      _id: singleEntry._id,
-                    },
-                  })
-                }}
-              >
-                Delete
-              </Button>
-            </Stack>
-          </Box>
-        ))}
-      </Container>
+      <DragDropContext
+        onDragEnd={() => {
+          handleDragEnd()
+        }}
+      >
+        <Container>
+          <h1>My entries</h1>
+          <Droppable droppableId="droppable">
+            {(provided, snapshot) => (
+              <div {...provided.droppableProps} ref={provided.innerRef}>
+                {entries?.map((item, index) => (
+                  <Draggable
+                    key={item._id}
+                    draggableId={item._id}
+                    index={index}
+                  >
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <Box
+                          display="flex"
+                          justifyContent="center"
+                          key={item._id}
+                          style={{ marginTop: '1rem' }}
+                        >
+                          <Stack direction="row" spacing={2}>
+                            <SingleEntry singleEntry={item} />
+                            <Button
+                              variant="outlined"
+                              color="error"
+                              onClick={() => {
+                                deleteEntry({
+                                  variables: {
+                                    _id: item._id,
+                                  },
+                                })
+                              }}
+                            >
+                              Delete
+                            </Button>
+                          </Stack>
+                        </Box>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </Container>
+      </DragDropContext>
     </LocalizationProvider>
   )
 }
