0bbd05dd5ed0b9a2bcf0e553be4e48db43a57f70
Mateusz Ryczek <ryczek.mateusz@gmail.com>
2021-10-26 18:32:36 +0200

    timePicker correctly show time

--- src/components/SingleEntry/SingleEntry.js
+++ src/components/SingleEntry/SingleEntry.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'
 import { TextField, MenuItem, Select } from '@mui/material'
-import { format } from 'date-fns'
 import { TimePicker } from '@mui/lab'
 
 export default function SingleEntry({ singleEntry }) {
-  const [startValue, setStartValue] = useState(new Date(singleEntry.createdAt))
-  const [endValue, setEndValue] = useState(singleEntry.endTime || undefined)
-  console.log(singleEntry.createdAt, new Date(singleEntry.createdAt))
-  console.log('start', startValue)
+  const startValues = singleEntry.startTime.split(':')
+  const dateObj = new Date(singleEntry.createdAt)
+  dateObj.setHours(startValues[0])
+  dateObj.setMinutes(startValues[1])
 
-  const minTime = new Date(singleEntry.createdAt)
+  const endValues = singleEntry.endTime.split(':')
+  const dateObjEnd = new Date(singleEntry.createdAt)
+  dateObjEnd.setHours(endValues[0])
+  dateObjEnd.setMinutes(endValues[1])
+
+  const [startValue, setStartValue] = useState(dateObj)
+  const [endValue, setEndValue] = useState(dateObjEnd || undefined)
+  // console.log(singleEntry)
+  // console.log(singleEntry.createdAt, new Date(singleEntry.createdAt))
+  // console.log('start', startValue)
 
   return (
     <>
@@ -28,7 +36,7 @@ export default function SingleEntry({ singleEntry }) {
         label="End time"
         value={endValue || startValue}
         ampm={false}
-        minTime={startValue && new Date(startValue)}
+        minTime={startValue}
         onChange={(newValue) => {
           setEndValue(newValue)
         }}
